84c1750c329d0a56a3973c49c6dd3d33247bad79
aidinabedi <aidinabedi@gmail.com>
2020-06-04 07:04:55 +0200

    add test case from englercj/tsd-jsdoc#105 to confirm that it's resolved

---
+++ test/expected/private_all.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Creates a new animal.
+ */
+declare class Animal {
+    /**
+     * Move the animal to the specified location.
+     * @param x - X-coordinate.
+     * @param y - Y-coordinate.
+     */
+    move(x: number, y: number): void;
+}
---
+++ test/specs/private.ts
@@ -0,0 +1,7 @@
+import { expectJsDoc } from '../lib';
+
+suite('Private Checks', () => {
+    test('All', () => {
+        expectJsDoc('private_all');
+    });
+});
